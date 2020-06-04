b3f8c34c061f1fcbb8a5624d9a9af8f213684e9e
peter <gudqo1995@naver.com>
2020-06-04 23:23:58 +0900

    test

--- Javaprj/src/ex0/test/test.java
+++ Javaprj/src/ex0/test/test.java
@@ -2,9 +2,13 @@ package ex0.test;
 
 public class test {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
-        for(int num : arr) {
-            System.out.println(num);
+//        int arr[] = {1, 2, 3, 4, 5};
+//        for(int num : arr) {
+//            System.out.println(num);
+        int a = 1;
+        int b = 2;
+    	System.out.printf("%d", b++);
+    	System.out.printf("%d", b);
         }
     }
-}
+
