7f8423730dcbc56b9f011c151b3228be6cc58c23
peter <gudqo1995@naver.com>
2020-07-31 20:47:05 +0900

    jdbc select table

--- JDBCProject/src/ex1/Program.java
+++ JDBCProject/src/ex1/Program.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class Program {
 
@@ -14,13 +15,27 @@ public class Program {
 		String sql = "SELECT * FROM NOTICE";
 		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection con = DriverManager.getConnection(url, "peterjeon0821", "0821"); //아이디 비번 까먹음
+		Connection con = DriverManager.getConnection(url, "peter", "0821"); //아이디 비번 까먹음
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(sql);
 		
-		rs.next();
-		String title = rs.getString("TITLE");
-		System.out.println(title);
+		while(rs.next()) {
+			int id = rs.getInt("ID");
+			String title = rs.getString("TITLE");
+			String WriterId = rs.getString("WRITER_ID");
+			Date regDate = rs.getDate("REGDATE");
+			String content = rs.getString("CONTENT");
+			int hit = rs.getInt("HIT");
+			
+			System.out.printf("id: %d\n", id);
+			System.out.printf("title: %s\n", title);
+			System.out.printf("WriterId: %s\n", WriterId);
+			System.out.printf("regDate: %s\n", regDate);
+			System.out.printf("content: %s\n", content);
+			System.out.printf("hit: %d\n", hit);
+			
+			System.out.println("------------------");
+		}
 		
 		rs.close();
 		st.close();
