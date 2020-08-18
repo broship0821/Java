09c8fe2351d9e99d57abf18c9695732e1a87a98a
gudqo1995 <gudqo1995@naver.com>
2020-08-18 18:22:32 +0900

    sql

--- registerjava/src/register/Main.java
+++ registerjava/src/register/Main.java
@@ -7,9 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		MemberDao dao = new MemberDao();
 		
+		//검색
 		Member m = dao.selectMember("hong");
 		System.out.println(m);
 		
+		//추가
 		Member m4 = new Member(++dao.seq, "peter", "1234", "피터");
 		dao.insertMember(m4);
 		
@@ -18,6 +20,7 @@ public class Main {
 		Member m2 = new Member(num, "qwer", "1234", "QQ");
 		dao.updateMember(num, m2);
 		
+		//삭제
 		dao.deleteMember(3);
 		
 		System.out.println("-----------------------------------------------");
