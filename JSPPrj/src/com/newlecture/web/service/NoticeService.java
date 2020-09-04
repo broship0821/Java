059ddcbee8d506aed577a33c3b84292abf6a5f86
peter <gudqo1995@naver.com>
2020-09-04 13:31:53 +0900

    admin page

--- JSPPrj/src/com/newlecture/web/service/NoticeService.java
+++ JSPPrj/src/com/newlecture/web/service/NoticeService.java
@@ -13,6 +13,35 @@ import com.newlecture.web.entity.Notice;
 import com.newlecture.web.entity.NoticeView;
 
 public class NoticeService {
+	public int removeNoticeAll(int[] ids){
+		
+		return 0;
+	}
+	
+	public int pubNoticeAll(int[] ids){
+		
+		return 0;
+	}
+	
+	public int insertNotice(Notice notice){
+		
+		return 0;
+	}
+	
+	public int deleteNotice(int id){
+		
+		return 0;
+	}
+	
+	public int updateNotice(Notice notice){
+		
+		return 0;
+	}
+	
+	public List<Notice> getNoticeNewestList(){
+		
+		return null;
+	}
 
 	//목록을 표시하는 메서드(초기값, 기본값)
 	public List<NoticeView> getNoticeList() {
