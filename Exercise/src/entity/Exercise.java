cab23c8472ac77fb952351b3b03ce90a194db11b
peter <gudqo1995@naver.com>
2020-09-28 21:18:42 +0900

    db 생성, entity만들기

---
+++ Exercise/src/entity/Exercise.java
@@ -0,0 +1,14 @@
+package entity;
+
+import java.sql.Date;
+
+public class Exercise {
+	private int pull_up;
+	private int hspu;
+	private int push_up;
+	private int samdu;
+	private int dips;
+	private int dumbbell_curl;
+	private int chin_up;
+	private Date regdate;
+}
