e3519e8b47013a0d067723e6613b94fb675ab883
peter <gudqo1995@naver.com>
2020-10-01 23:22:58 +0900

    connection pool 만들기2

--- Exercise/src/service/DAO.java
+++ Exercise/src/service/DAO.java
@@ -1,5 +1,14 @@
 package service;
 
 public class DAO {
-
+	private static DAO instance = null;
+	private DAO() {}
+	public static DAO getInstance() {
+		if(instance==null) {
+			synchronized (DAO.class) {
+				instance = new DAO();
+			}
+		}
+		return instance;
+	}
 }
