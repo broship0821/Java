226492b1bbf011897c995304762cfc44a809be65
peter <gudqo1995@naver.com>
2020-11-27 16:45:03 +0900

    spring

--- myboard/src/main/java/peter/spring/lifecycle/MyLifeBeanMain.java
+++ myboard/src/main/java/peter/spring/lifecycle/MyLifeBeanMain.java
@@ -1,18 +1,25 @@
 package peter.spring.lifecycle;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class MyLifeBeanMain {
 
 	public static void main(String[] args) {
-		FileSystemResource resource =
-				new FileSystemResource("src/main/resources/applicationContext.xml");
-		XmlBeanFactory factory = new XmlBeanFactory(resource);
-		factory.addBeanPostProcessor(new CustomBeanPostProcessor());
+//		FileSystemResource resource =
+//				new FileSystemResource("src/main/resources/applicationContext.xml");
+//		XmlBeanFactory factory = new XmlBeanFactory(resource);
+//		factory.addBeanPostProcessor(new CustomBeanPostProcessor());
+		
+//		factory.destroyBean("myLifeBean", bean);
+		
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
 		MyLifeBean bean = (MyLifeBean) factory.getBean("myLifeBean");
 		bean.sayHello();
-		factory.destroyBean("myLifeBean", bean);
+		factory.close();
 
 	}
 
